1d317410daa008103c9f3ea9d472e11a7128260d
Vlad <vladmititelu1@gmail.com>
2020-10-25 09:52:49 +0200

    Refactor to async/await

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -2,36 +2,35 @@ const Product = require('../models/product');
 
 const mongoose = require('mongoose');
 
-exports.products_get_all = (req, res, next) => {
-    Product.find()
-        .select('name price _id productImage')
-        .exec()
-        .then(docs => {
-            const response = {
-                count: docs.length,
-                products: docs.map(doc => {
-                    return {
-                        name: doc.name,
-                        price: doc.price,
-                        productImage: doc.productImage,
-                        _id: doc._id,
-                        request: {
-                            type: 'GET',
-                            url: 'http://localhost:3000/products/' + doc._id
-                        }
+exports.products_get_all = async (req, res, next) => {
+    try{
+        const docs = await Product.find().select('name price _id productImage');
+        const response = {
+            count: docs.length,
+            products: docs.map(doc => {
+                return {
+                    name: doc.name,
+                    price: doc.price,
+                    productImage: doc.productImage,
+                    _id: doc._id,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:4000/products/' + doc._id
                     }
-                })
-            };
+                }
+            })
+        };
+        
+        res.status(200).json({response});
 
-            res.status(200).json({response})
-        })
-        .catch(err => {
-            console.log(err);
+    }
+    catch(err){
+        console.log(err);
             res.status(500).json({error: err})
-        })
+    }
 };
 
-exports.products_create_product = (req, res, next) => {
+exports.products_create_product = async(req, res, next) => {
 
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
@@ -40,102 +39,99 @@ exports.products_create_product = (req, res, next) => {
         productImage: req.file.path
     });
 
-    product
-        .save()
-        .then(result => {
-            console.log(result);
-            res.status(201).json({
-                message: 'Created product successfully',
-                createdProduct: {
-                    name : result.name,
-                    price: result.price,
-                    _id : result._id,
-                    productImage: result.productImage,
-                    request: {
-                        type: "GET",
-                        url: "http://localhost:3000/" + result._id
-                    }
-                }
-            })
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            })
-        })  
-};
-
-exports.products_get_product = (req, res, next) => {
-    const id = req.params.productId;
-    Product.findById(id)
-        .select('name price _id productImage')
-        .exec()
-        .then(doc => {
-            console.log(doc);
-            if(doc)
-            {
-                res.status(200).json({
-                    product: doc,
-                    request: {
-                        type: "GET",
-                        description: "Get all products",
-                        url: "localhost:3000/products"
-                    }
-                })
-            }
-            else{
-                res.status(404).json({message: "No json object found for provided id"})
-            }
-            
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({error: err})
-        })
-};
-
-exports.products_update_product = (req, res, next) => {
-    const id = req.params.productId;
-    const updateOps = {};
-    for (const ops of req.body){
-        updateOps[ops.propName] = ops.value;
-    }
-    Product.update({_id: id}, {$set: updateOps})
-        .exec()
-        .then(result => {
-            console.log(result);
-            res.status(200).json({
-                message: "Product updated",
+    try{
+        const result = await product.save();
+        res.status(201).json({
+            message: 'Created product successfully',
+            createdProduct: {
+                name : result.name,
+                price: result.price,
+                _id : result._id,
+                productImage: result.productImage,
                 request: {
                     type: "GET",
-                    url: "http://localhost:3000/products/"+ id
+                    url: "http://localhost:4000/" + result._id
                 }
-            });
+            }
         })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({error: err})
-        })
+    }
+    catch(err){
+        res.status(500).json({error: err})
+    }
 };
 
-exports.products_delete_product = (req, res, next) => {
+exports.products_get_product = async (req, res, next) => {
     const id = req.params.productId;
-    Product.remove({_id: id})
-        .exec()
-        .then(result => {
-            console.log(result);
+
+    try{
+        const doc = await Product.findById(id).select('name price _id productImage');
+        if(doc)
+        {
             res.status(200).json({
-                message: "Product deleted",
+                product: doc,
                 request: {
-                    type: 'POST',
-                    url: 'http://localhost:3000/products',
-                    body: { name: 'String', price: 'Number'}
-                }
+                    type: "GET",
+                    description: "Get all products",
+                    url: "http://localhost:4000/products"
+                    }
             })
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({error: err})
+        }
+        else {
+            res.status(404).json({message: "No json object found for provided id"})
+        }
+    }
+    catch(err){
+        res.status(500).json({error: err})
+    }
+};
+
+exports.products_update_product = async (req, res, next) => {
+    const id = req.params.productId;
+    const updateOps = {};
+    const availableProperties = Object.keys(Product.schema.paths);
+
+    for (const ops of req.body){
+        if(availableProperties.includes(ops.propName))
+            updateOps[ops.propName] = ops.value;
+    }
+
+    if(Object.keys(updateOps).length === 0 && updateOps.constructor === Object)
+    {
+        res.status(500).json({message: "Body is empty, no update operations found"})
+    }
+
+    try{
+        await Product.update({_id: id}, {$set: updateOps});
+        await res.status(200).json({
+            message: "Product updated",
+            request: {
+                type: "GET",
+                url: "http://localhost:4000/products/"+ id
+            }
         });
+    }
+    catch(err){
+        await res.status(500).json({error: err})
+    }
+
+};
+
+exports.products_delete_product = async (req, res, next) => {
+    const id = req.params.productId;
+
+    //TODO: check if id exists
+    try{
+        await Product.remove({_id: id});
+        await res.status(200).json({
+            message: "Product deleted",
+            request: {
+                type: 'POST',
+                url: 'http://localhost:4000/products',
+                body: { name: 'String', price: 'Number'}
+            }
+        })
+    }
+    catch(err){
+        res.status(500).json({error: err});
+    }
 };
