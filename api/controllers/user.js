32051235af1f4647ac7df13e29f2eec6e1bb5e30
Vlad <vladmititelu1@gmail.com>
2020-10-26 22:34:36 +0200

    Refactor async/await

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,61 +5,52 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 
-exports.users_create_user = (req, res, next) => {
+exports.users_create_user = async (req, res, next) => {
+    try{
+        const user = await User.find({email: req.body.email});
+        if(user.length >= 1){
+            return res.status(409).json({
+                message: 'Mail exists'
+            })
+        }
+        else{
+            const hash = await bcrypt.hash(req.body.password, 10);
 
-    User.find({email: req.body.email}).exec()
-        .then(user => {
-            if(user.length >= 1){
-                return res.status(409).json({
-                    message: 'Mail exists'
-                })
-            }
-            else{
-                bcrypt.hash(req.body.password, 10, (err, hash) => {
-                    if(err){
-                        res.status(500).json({error: err})
-                    }
-                    else{
-                        const user = new User({
-                            _id: new mongoose.Types.ObjectId(),
-                            email:  req.body.email,
-                            password: hash
-                        });
-            
-                        user.save()
-                            .then(result => {
-                                console.log(result)
-                                res.status(201).json({message: 'User created'})
-                            })  
-                            .catch(err => {
-                                console.log(err)
-                                res.status(500).json({error: err})
-                        });
-                    }
-                    
-                })
-            }
-        })
+            const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                email:  req.body.email,
+                password: hash
+            });
+
+            await user.save();
+
+            res.status(201).json({message: 'User created'});
+        }
+    }
+    catch(err){
+        res.status(500).json({error: err});
+    }
 };
 
-exports.users_delete_user = (req, res, next) => {
-    User.remove({_id: req.params._id})
-        .exec()
-        .then(result => {
-            res.status(200).json({message: 'User deleted'})
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json({error: err})
-        })
+exports.users_delete_user = async (req, res, next) => {
+
+    try{
+        await User.remove({_id: req.params._id});
+        res.status(200).json({message: 'User deleted'});
+    }
+    catch(err){
+        res.status(500).json({error: err})
+    }
 };
 
-exports.users_authenticate_user = (req, res, next) => {
-    User.find({email: req.body.email}).exec()
-        .then(user => {
-            if(user.length < 1)
-                return res.status(401).json({message: "Auth failed"});
+exports.users_authenticate_user = async (req, res, next) => {
 
+    try{
+        const user = await User.find({email: req.body.email});
+        if(user.length < 1){
+            return res.status(401).json({message: "Auth failed"});
+        }
+        else{
             bcrypt.compare(req.body.password, user[0].password, (err, result) => {
                 if(err)
                     return res.status(401).json({message: "Auth failed"});
@@ -79,9 +70,9 @@ exports.users_authenticate_user = (req, res, next) => {
 
                 res.status(401).json({message: "Auth failed"});
             })
-        })
-        .catch(err => {
-        console.log(err)
+        }
+    }
+    catch(err){
         res.status(500).json({error: err})
-    })
+    }
 };
