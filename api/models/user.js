99771820ed861465e4217e0c5ac28ffb49267d14
Vlad <vladmititelu1@gmail.com>
2020-10-27 12:37:26 +0200

    Add new property to user

--- api/models/user.js
+++ api/models/user.js
@@ -9,7 +9,8 @@ const userSchema = mongoose.Schema({
         unique: true,
         match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     },
-    password : {type: String, required: true}
+    password : {type: String, required: true},
+    role: {type: String, default: 'normal', enum: ['admin', 'normal']}
 });
 
 module.exports = mongoose.model('User', userSchema)
