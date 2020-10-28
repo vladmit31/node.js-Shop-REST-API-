6e5cab5bc469771e7c0a5b3c2cfeeefb6729d817
Vlad <vladmititelu1@gmail.com>
2020-10-28 17:04:57 +0200

    whitespaces

--- api/routes/user.js
+++ api/routes/user.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const UserController = require('../controllers/user');
 const checkAuth = require('../middleware/check-auth');
 
+
 router.post('/signup', UserController.users_create_user);
 
 router.delete('/:userId', checkAuth, UserController.users_delete_user);
